463f6dd8051f33cf4bcdd62b3cce0923417a4bdd
yxnyxnyxnyxnyxnyxn <yxn1116@gmail.com>
2022-12-12 20:34:14 -0500

    add write methods

--- read_write.py
+++ read_write.py
@@ -1,7 +1,8 @@
 #Contants 
 READ_TEST_PATH = "read_test.txt"
+WRITE1_PATH = "write_test1.txt"
 
-#Method1: Using readline
+#Read Method1: Using readline
 def read1(path):
 	f = open(path, 'r')
 	line = f.readline() 
@@ -12,12 +13,25 @@ def read1(path):
 
 
 
-#Method2: Using for loop and read 
+#Read Method2: Using for loop and read 
 def read2(path):
 	lines = open(path,'r')
 	for  line in lines:
 		print(line)
 	lines.close()
+
+
+#Write Method1: Overriate entire file
+def write1(path,content):
+	f = open(path, 'w')
+	f.write(content)
+	f.close() 
+
+#Write Method2: Add content to the file 
+def write2(path, content):
+	f = open(path, 'a')
+	f.write(content)
+	f.close()
 
 def main():
 	
@@ -30,6 +44,16 @@ def main():
 	print("---------Read Method 2-----------")
 
 	read2(READ_TEST_PATH)
+
+	print("---------Write overwrite entire file --------")
+	test_lines = "Line 1\nLine 2\nWrite 1\nWrite 2"
+	write1(WRITE1_PATH, test_lines) 
+
+
+	print("---------Write add to file --------")
+	test_lines2 = "\nAppend 1\nAppend 2\nOver"
+	write2(WRITE1_PATH, test_lines2)
+
 
 
 
